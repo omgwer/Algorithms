4b4c8068f6d90557769401bf802eb90d42d2861f
Aleksandr Kislitsyn <aleksandr.kislitsyn0@gmail.com>
2024-04-07 23:16:43 +0300

    add studip sort

--- Algorithms/Algorithms/Sort/BubbleSort.cs
+++ Algorithms/Algorithms/Sort/BubbleSort.cs
@@ -4,7 +4,6 @@ public static class BubbleSort
 {
     public static int[] Sort(this int[] values)
     {
-         
         return values;
     }
 }
---
+++ Algorithms/Algorithms/Sort/StupidSort.cs
@@ -0,0 +1,9 @@
+﻿namespace Algorithms.Algorithms.Sort;
+
+public static class StupidSort
+{
+    public static int[] Sort(this int[] value)
+    {
+        return value;
+    }
+}
